541ef3ed87a5a4de4d8fb3450970983baa40f5d2
Bob Parsons <deadstylebp@gmail.com>
2024-07-22 19:56:26 -0500

    Fix prod name

--- init_production.sql
+++ init_production.sql
@@ -1,11 +1,11 @@
 -- Create the new database
-CREATE DATABASE ${PRODUCTION_DB_NAME};
+CREATE DATABASE ${PROD_DB_NAME};
 
 -- Create a new superuser
-CREATE USER ${PRODUCTION_DB_USER} WITH PASSWORD '${PRODUCTION_DB_PASSWORD}' SUPERUSER;
+CREATE USER ${PROD_DB_USER} WITH PASSWORD '${PROD_DB_PASSWORD}' SUPERUSER;
 
 -- Grant privileges on the new database
-GRANT ALL PRIVILEGES ON DATABASE ${PRODUCTION_DB_NAME} TO ${PRODUCTION_DB_USER};
+GRANT ALL PRIVILEGES ON DATABASE ${PROD_DB_NAME} TO ${PROD_DB_USER};
 
 -- Connect to 'postgres' database to perform administrative actions
 \c postgres
@@ -15,4 +15,4 @@ DROP USER IF EXISTS postgres;
 
 -- Optional: Revoke public privileges
 REVOKE ALL ON SCHEMA public FROM PUBLIC;
-GRANT ALL ON SCHEMA public TO ${PRODUCTION_DB_USER};
+GRANT ALL ON SCHEMA public TO ${PROD_DB_USER};
