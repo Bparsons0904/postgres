8de44474546e66ebaf40e929be8bcd8cf51833e9
Bob Parsons <deadstylebp@gmail.com>
2024-08-14 16:40:06 -0500

    add logging

--- init-extensions.sql
+++ init-extensions.sql
@@ -1,2 +1,6 @@
+\echo 'Starting initialization script'
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+\echo 'uuid-ossp extension created'
 CREATE EXTENSION IF NOT EXISTS "pg_uuidv7";
+\echo 'pg_uuidv7 extension created'
+\echo 'Initialization script completed'
